05246ecb689dad3ffd68d00eccb8beb68e5b9cbd
Alemnew Marie <alemnewmarie461@gmail.com>
2024-05-28 21:27:46 +0300

    improved error message

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,18 @@
+/* eslint-disable react/prop-types */
 import { useState, useEffect } from 'react'
 import { getAll, createPerson, updatePerson, deletePerson } from './services/phonebook'
 
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={type}>
+      {message}
+    </div>
+  )
+}
 
 const Filter = ({ query, handleSearch }) => {
   return (
@@ -57,6 +69,8 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [query, setQuery] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -79,14 +93,47 @@ const App = () => {
         const person = persons.find(person => person.name === newName)
         const changedPerson = { ...person, number: newPhone }
         updatePerson(person.id, changedPerson)
-          .then(data => setPersons(persons.map(person => person.id !== data.id ? person : data)))
+          .then(data => {
+            setPersons(persons.map(person => person.id !== data.id ? person : data))
+
+            setSuccessMessage(`Number changed for ${newName}`)
+
+            setTimeout(() => {
+              setSuccessMessage(null)
+            }, 5000)
+          })
+          .catch(error => {
+            setErrorMessage(`Information of ${newName} has already been removed from server`)
+
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000);
+          })
+
+        setNewName('')
+        setNewPhone('')
       }
       return
     }
 
     const newPerson = { name: newName, number: newPhone }
     createPerson(newPerson)
-      .then(data => setPersons(persons.concat(data)))
+      .then(data => {
+        setPersons(persons.concat(data))
+
+        setSuccessMessage(`Added ${newName}`)
+
+        setTimeout(() => {
+          setSuccessMessage(null)
+        }, 5000)
+      })
+      .catch(error => {
+        setErrorMessage(error.response.data.error)
+
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000);
+      })
 
     setNewName('')
     setNewPhone('')
@@ -97,6 +144,13 @@ const App = () => {
     if (window.confirm(`Delete ${person.name}?`)) {
       deletePerson(id)
         .then(() => setPersons(persons.filter(person => person.id !== id)))
+        .catch(error => {
+          setErrorMessage(`Information of ${person.name} has already been removed from server`)
+
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000);
+        })
     }
   }
 
@@ -113,6 +167,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Notification message={errorMessage} type={'error'} />
+      <Notification message={successMessage} type={'success'} />
 
       <Filter
         query={query}
